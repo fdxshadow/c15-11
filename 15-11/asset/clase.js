4f8debd421338dd8b167f24fd7901c86953f255e
Francisco <fran.peagonza@gmail.com>
2016-11-15 13:09:37 -0300

    clase 15-11

---
+++ 15-11/asset/clase.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$("#btn").click(function(){
+	$("#texto").html("mi texto");
+
+	$("#btn2").click(function(){
+		$("#texto").css("color","blue");
+	});
+
+
+
+
+});
+
+
+
+		})
